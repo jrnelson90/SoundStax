8c89d1976874528f4908aad080e4dd6e710521f9
jrnelson90 <jrnelson90@gmail.com>
2017-04-28 16:15:41 -0600

    Begin name change to SoundStax

--- app/src/main/java/com/justinrandalnelson/waxstacks/HttpConst.java
+++ app/src/main/java/com/justinrandalnelson/waxstacks/HttpConst.java
@@ -10,6 +10,6 @@ public class HttpConst {
     public static final String ACCESS_TOKEN_ENDPOINT_URL = "https://api.discogs.com/oauth/access_token";
     public static final String CONSUMER_KEY = "XqiFKQsXTFMDJpxWsrue";
     public static final String CONSUMER_SECRET = "XjWXVtNXjieFeGMJoHDCvbnibIOBqkbu";
-    public static final String CALLBACK_URL = "callback://waxstacks";
+    public static final String CALLBACK_URL = "callback://soundstax";
     public static final String USER_AGENT = "waxstacks/0.1 +com.justinrandalnelson.waxstacks";
 }
