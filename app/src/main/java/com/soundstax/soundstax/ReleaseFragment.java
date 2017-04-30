8dc6f6cda91ec98bd9f60c8db3f118046f3a4de5
jrnelson90 <jrnelson90@gmail.com>
2017-04-29 18:20:20 -0600

    Intial fixes to string lengths.

--- app/src/main/java/com/soundstax/soundstax/ReleaseFragment.java
+++ app/src/main/java/com/soundstax/soundstax/ReleaseFragment.java
@@ -7,6 +7,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -177,6 +178,8 @@ public class ReleaseFragment extends Fragment {
 
                     String trackNameString = currentTrack.getString("title");
                     trackName.setText(trackNameString);
+                    trackName.setEllipsize(TextUtils.TruncateAt.END);
+                    trackName.setMaxLines(1);
 
                     String trackDurationString = currentTrack.getString("duration");
                     if (trackDurationString.length() == 0) {
